91305c6c7031288443c58ca03d41f898659a1749
lcdevr <laurencercross@outlook.com>
2024-08-07 09:07:37 +0100

    scrape goalkeeper initial

--- main.py
+++ main.py
@@ -13,15 +13,25 @@ def scrape_so_matches(i_filepath):
         so_match_link = x[3]
         o_filepath = f'data/{season}/{league}/so_matches/{team}.csv'
         file_util.check_delete_file(o_filepath)
-        scraper.scrape_matches(so_match_link,team,league,o_filepath)
+        scraper.scrape_so_matches(so_match_link,team,league,o_filepath)
         time.sleep(8)
+        
+def scrape_so_goalkeeper(i_filepath):
+    # data = file_util.open_csv(i_filepath)
+    data = 'a'
+    for x in data:
+        season = '23_24'# x[0]
+        league = 'eng_prem_league' # x[1]
+        team = 'West Ham'# x[2]
+        so_goalkeeper_link = 'https://fbref.com/en/squads/7c21e445/2023-2024/West-Ham-United-Stats' # x[3]
+        o_filepath = f'data/{season}/{league}/so_goalkeepers/{team}.csv'
+        # file_util.check_delete_file(o_filepath)
+        scraper.scrape_so_goalkeeper(so_goalkeeper_link,team,league,o_filepath)
         
 def main():
     i_filepath = 'lookups/23_24/squad_overview.csv'
-    scrape_so_matches(i_filepath)
+    # scrape_so_matches(i_filepath)
+    scrape_so_goalkeeper(i_filepath)
 
 if __name__ == "__main__":
     main()
-    
-
-# f'data\{season}\{league}\so_matches\{team}'
--- utilities/scraper.py
+++ utilities/scraper.py
@@ -1,18 +1,25 @@
 import pandas as pd
 
-def scrape_matches(url, teamname, league, o_filepath):
+def scrape_so_matches(url, teamname, league, o_filepath):
     df = pd.read_html(url)[1]
     df.insert(loc=0,column='Team',value=teamname)
     df.to_csv(o_filepath)
 
-def scrape_goalkeeper():
-    pass
+def scrape_so_goalkeeper(url, teamname, league, o_filepath):
+    df = pd.read_html(url)[2]
+    df = df.iloc[:2]
+    df.insert(loc=0,column='Team',value=teamname)
+    df = df.reset_index(drop=True)
+    print(df)
 
-def scrape_shooting():
-    pass
+def scrape_so_shooting(url, teamname, league, o_filepath):
+    df = pd.read_html(url)[3]
+    print(df)
 
-def scrape_playing():
-    pass
+def scrape_so_playing(url, teamname, league, o_filepath):
+    df = pd.read_html(url)[4]
+    print(df)
 
-def scrape_misc():
-    pass
+def scrape_so_misc(url, teamname, league, o_filepath):
+    df = pd.read_html(url)[5]
+    print(df)
